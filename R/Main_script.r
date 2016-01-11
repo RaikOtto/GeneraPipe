232d57affafacbf713aa89dce5448d35de13d683
Jan-Niklas Rössler <jan-niklas.roessler@fu-berlin.de>
2016-01-11 16:05:18 +0100

    update

--- R/Main_script.r
+++ R/Main_script.r
@@ -1,59 +1,20 @@
 #' @export
-start_analysis = function(){
-  kocent = ( system( 'uname -n', intern = T ) == "kocent" )
-
-  debug_mode = F
-
-  if ( kocent ){
-
-    pipeline_loc = "/usr/Generic_mRNA_Expression_Pipeline" #server path
-
-  } else if ( system( 'uname -n', intern = T ) == "MacBook-Jan-Niklas.local" ){
-
-    pipeline_loc = paste( system( "echo $HOME", intern = T), "Generic_mRNA_Expression_Pipeline", sep ="/" )
-
+start_analysis = function(cel_files_path = "", project = "GSE14407"){
+  
+  package_path = system.file("", package = "GeneraPipe")
+  database_path = paste(package_path, "GeneraPipeDefaultDB.sqlite3", sep = "/")
+  
+  #load project parameters
+  project_para = data.frame(dplyr::filter(tbl(src_sqlite(database_path), "projects"), ID == project))
+  
+  #extract parameters
+  chip_type = project_para$chip_type
+  if (project_para$zipped == 1){
+    zipped = TRUE
   } else{
-
-    pipeline_loc = "/Users/raikotto/Dropbox/PhD/Generic_Biomarker_mRNA_Pipeline/"
-
+    zipped = FALSE
   }
+  
+  parse_cel_files(cel_files_path, chip_type, zipped)
 
-  #pipeline_loc = paste( system( "echo $HOME", intern = T), "Generic_mRNA_Expression_Pipeline", sep ="/" )
-  print(c("Kocent:",kocent ))
-
-  setwd( pipeline_loc ) # Set the path to where the pipeline is located
-
-  #options(error=traceback)
-  default_parameters = T
-
-  which_project = "MZ_SM_all"
-
-  source("project_files.r")
-  options(error=traceback)
-  source( "Src/pipeline_structure.r" )
-
-
-  ### MZ_SM_all, MZlike_all, SM_all, SM_MZ_no_sici, MZ_no_sici, SM_no_sici
-  var_filter = F
-
-  p_val = 0.05
-  create_heatmaps_genes_of_interest = T
-  use_kegg_for_heatmap = T
-
-  create_cohorts    = T # 2
-  parse_files       = T # 3
-  normalize         = T # 4
-  qc_control        = F # 5
-  annotate          = T # 6
-  absent_analysis   = F # 7
-  dif_exp_ana       = T # 8
-  export_results    = T # 9
-  create_pathways   = F # 10
-  extract_interest  = T # 11
-  annotate_tissue_abbundance = F # 12
-
-  ### generic
-  stat_design = "contrast"
-
-  run_analysis();print( "Finished" )
 }
---
+++ R/parse_cel_files.R
@@ -0,0 +1,40 @@
+read_cel_files = function(cel_files_path, chip_type, zipped){
+  
+  if ( chip_type == "HumanHT-12.v4" ){
+  
+    raw_data = getGEO(GEO_Id, GSEMatrix = T)
+    if ( length( raw_data ) > 1 )
+      idx = grep("GPL10558", attr(raw_data, "names"))
+    else
+      idx = 1
+    raw_data = raw_data[[idx]]
+  
+  } else if ( chip_type %in% c( "hgu133plus2", "hgu133a" ) ){
+  
+    if ( zipped ){
+      celFiles = list.celfiles( cel_files_path, full =T)
+    } else {
+      celFiles = list.celfiles( cel_files_path, full =T)
+    }
+  
+    raw_data = read.affybatch( filenames = celFiles )
+    #excluded_files = c()
+  
+    if( project_name == "Plus2_Runx"  ){
+      excluded_files = c( 1,2,3,21,94,115,116,117,118,119,120,121,122,123,124,125,129,138,153,154,155,156,157,158,159,160,161,164,165,174,175,176,179)
+      raw_data = raw_data[ -( excluded_files )  ] # exclude bad samples
+      phenodata = phenodata[ match( phenodata$ID, colnames(raw_data), nomatch = 0 ),]
+    }
+  
+  } else if ( chip_type %in% c( "pd.huex.1.0.st.v2", "pd.hugene.2.0.st" ) ){
+  
+    library("oligo")
+    celFiles = list.celfiles( cel_files_path, full = T, listGzipped = zipped)
+    raw_data = read.celfiles( filenames = celFiles )
+  
+  } else {
+  
+    message(c("Unknown Chip Type: ",chip_type))
+    stop()
+  }
+}
