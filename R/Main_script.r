8dae14f893a00b3f7b3ed216e422c4ba6c8ce0ff
Jan-Niklas Rössler <jan-niklas.roessler@fu-berlin.de>
2016-02-29 15:42:10 +0100

    update

--- R/Main_script.r
+++ R/Main_script.r
@@ -1,46 +1,95 @@
 #' @export
-start_analysis = function(cel_files_path = "", output_path = cel_files_path, project = "GSE14407"){
+start_analysis = function(
+  cel_files_path = "", 
+  output_path = cel_files_path,
+  database_path = "~/GeneraPipe/inst/GeneraPipeDefaultDB.sqlite3",
+  project = "GSE29156",
+  gene_set_database_path = ""
+  ){
   
-  # cel_files_path = "~/Generic_mRNA_Expression_Pipeline/Project_files/GSE14407_RAW/Input/"
-  # output_path = "~/Documents"
+  #cel_files_path = "~/Documents/GeneraPipe_GSE29156/Input"
+  #output_path = "~/Documents"
   package_path = system.file("", package = "GeneraPipe")
-  database_path = "~/GeneraPipe/inst/GeneraPipeDefaultDB.sqlite3"
+  #database_path = "~/GeneraPipe/inst/GeneraPipeDefaultDB.sqlite3"
   #database_path = paste(package_path, "GeneraPipeDefaultDB.sqlite3", sep = "/")
   
   message( paste( "Running GeneraPipe with project ", project, ".", sep = "" ) )
   #load project parameters
-  project_para = data.frame(dplyr::filter(tbl(src_sqlite(database_path), "projects"), ID == project))
+  project_para = data.frame(dplyr::filter(dplyr::tbl(dplyr::src_sqlite(database_path), "projects"), ID == project))
   
   #set project parameters
-  message( "Setting project parameters." )
-  package_env$project_name = project_para$project_name
-  chip_type                = project_para$chip_type
-  package_env$kegg_file    = project_para$kegg_file
-  package_env$cohorts_file = project_para$cohorts_file
-  set_case                 = project_para$set_case
-  set_ctrl                 = project_para$set_ctrl
-  package_env$p_val        = project_para$p_val
-  package_env$lfc_exp      = project_para$lfc_exp
-  zipped                   = as.logical(project_para$zipped)
+  message("Setting project parameters.")
+  project_name = project_para$project_name
+  chip_type    = project_para$chip_type
+  kegg_file    = project_para$kegg_file
+  cohorts_file = project_para$cohorts_file
+  set_case     = project_para$set_case
+  set_ctrl     = project_para$set_ctrl
+  p_val        = project_para$p_val
+  lfc_exp      = project_para$lfc_exp
+  zipped       = as.logical(project_para$zipped)
   
   # set default parameters
   message( "Configurating workspace, loading input data.")  
-  GeneraPipe::set_initial_parameters(cel_files_path, output_path, database_path)
+  phenodata = GeneraPipe::set_initial_parameters(cel_files_path, output_path, database_path, project_name, cohorts_file, kegg_file, package_path)
   
   # read in CEL files
   message( "Reading in CEL files." )
-  raw_data = GeneraPipe::read_cel_files(cel_files_path, chip_type, project_name, zipped)
+  raw_data = GeneraPipe::read_cel_files(cel_files_path, chip_type, zipped)
   
   # create cohorts
   message( "Creating cohorts for data.")
-  raw_data = GeneraPipe::create_cohorts( raw_data, chip_type, set_ctrl, set_case)
+  raw_data = GeneraPipe::create_cohorts( raw_data, chip_type, set_ctrl, set_case, phenodata)
+  phenodata = env$phenodata
   
-  # Normalization
+  if (env$create_heatmaps_genes_of_interest){
+    if (env$use_kegg_for_heatmap){
+      message("Creating heatmaps for genes of interest.")
+    } else{
+      message(paste("Creating heatmaps for the ", env$heatmap_list_genes_count, " highest differential expressed genes.", sep = ""))
+    }
+  }
+  
+  # normalization
   message("Normalizing raw data.")
   eset = GeneraPipe::normalize(raw_data = raw_data, chip_type = chip_type, zipped = zipped)
   
-  # Annotation
-  message( "Annotating data." )
-  GeneraPipe::annotate( eset, chip_type )
+  # annotation
+  message("Annotating data.")
+  eset = GeneraPipe::annotate( eset, chip_type, annotate )
+  
+  # differential expression analysis
+  message("Performing differential expression analysis.")
+  topall = GeneraPipe::dif_exp_analysis( eset, chip_type, phenodata, p_val, lfc_exp )
+  eset = env$eset
+  volc_all = env$volc_all
+  
+  # export results
+  message(paste("Exporting results to ", env$results_file_path, ".", sep = ""))
+  topall_res = GeneraPipe::result_preparation( eset, topall, chip_type, lfc_exp )
+  eset = env$eset
+  
+  # compute GSEA
+  if (env$use_gsea){
+    GeneraPipe::compute_gsea( cel_files_path, eset, set_case, set_ctrl, gene_set_database_path )
+  }
+  
+  # create pathway maps
+  message(paste("Creating pathway maps, exporting results to ", env$pathway_maps_path, ".", sep = ""))
+  #GeneraPipe::create_pathway_maps( topall_res, eset, volc_all, chip_type, package_path )
+  
+  # extract interesting entities
+  message(paste("Computing results for interesting entities, exporting results to ", env$entities_of_interest_path, ".", sep = "."))
+  #GeneraPipe::extract_intr_entities( eset, chip_type )
+  
+  # create heatmaps
+  if (env$create_heatmaps_genes_of_interest){
+    if (env$use_kegg_for_heatmap){
+      message("Creating heatmaps for genes of interest.")
+    } else{
+      message(paste("Creating heatmaps for the ", env$heatmap_list_genes_count, " highest differential expressed genes.", sep = ""))
+    }
+    GeneraPipe::create_heatmaps( eset, topall_res, project_name, set_ctrl, set_case )
+  }
   
 }
--- R/parse_cel_files.R
+++ R/parse_cel_files.R
@@ -1,4 +1,4 @@
-read_cel_files = function(cel_files_path, chip_type, project_name, zipped){
+read_cel_files = function( cel_files_path, chip_type, zipped ){
   
   if ( chip_type == "HumanHT-12.v4" ){
   
@@ -15,16 +15,8 @@ read_cel_files = function(cel_files_path, chip_type, project_name, zipped){
       celFiles = affy::list.celfiles( cel_files_path, full = TRUE)
     } else {
       celFiles = affy::list.celfiles( cel_files_path, full = TRUE)
-    }
-  
+    }  
     raw_data = affy::read.affybatch( filenames = celFiles )
-    #excluded_files = c()
-  
-    if( package_env$project_name == "Plus2_Runx"  ){
-      excluded_files = c( 1,2,3,21,94,115,116,117,118,119,120,121,122,123,124,125,129,138,153,154,155,156,157,158,159,160,161,164,165,174,175,176,179)
-      raw_data = raw_data[ -( excluded_files )  ] # exclude bad samples
-      phenodata = phenodata[ match( phenodata$ID, colnames(raw_data), nomatch = 0 ),]
-    }
   
   } else if ( chip_type %in% c( "pd.huex.1.0.st.v2", "pd.hugene.2.0.st" ) ){
   
